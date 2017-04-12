b954fa966c0b4e36055cbd31e302d8e98fef7f19
shunshun <lwwtotod@gmail.com>
2017-04-12 21:22:48 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- <router-view/> -->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import './common/stylus/variable';
+
+#app {
+  color: $color-theme;
+}
+</style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="m-header">
+      <div class="icon"></div>
+        <h1 class="text">chicken music</h1>
+  </div>
+</template>
+<script>
+    export default{}
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+
+</style>
